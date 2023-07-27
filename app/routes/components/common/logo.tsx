98f2b9175985ca8a5ebe6bae3a00285eb67b3c57
niwatoro <girlwithskyhairpin@gmail.com>
2023-07-27 10:11:13 +0900

    replace icon

--- app/routes/components/common/logo.tsx
+++ app/routes/components/common/logo.tsx
@@ -10,7 +10,7 @@ export const Logo = () => {
           <div>望</div>
           <div>月</div>
         </div>
-        <div className={"h-full border border-white"} />
+        <div className={"h-full border-[0.5px] border-white"} />
         <div>
           <div>文</div>
           <div>庫</div>
