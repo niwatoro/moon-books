77568e1f981eb405fc65a244d9dda38fdbb7c671
niwatoro <girlwithskyhairpin@gmail.com>
2023-07-27 22:20:45 +0900

    update logo & icon

--- app/routes/components/logo.tsx
+++ app/routes/components/logo.tsx
@@ -3,17 +3,17 @@ export const Logo = () => {
     <div className={"bg-black p-1"}>
       <div
         className={
-          "font-hina border border-white h-20 bg-black text-white text-3xl flex items-center"
+          "font-hina border border-white h-16 leading-none bg-black text-white text-3xl flex items-center"
         }
       >
-        <div className={"px-0.5"}>
-          <div>望</div>
-          <div>月</div>
-        </div>
-        <div className={"h-full border-[0.5px] border-white"} />
-        <div className={"px-0.5"}>
+        <div>
           <div>文</div>
           <div>庫</div>
+        </div>
+        <div className={"h-full border-[0.5px] border-white"} />
+        <div>
+          <div>望</div>
+          <div>月</div>
         </div>
       </div>
     </div>
