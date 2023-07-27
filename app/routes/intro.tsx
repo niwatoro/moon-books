f86acc72e7f3202cba69b11a2372d49a582c2ca0
niwatoro <girlwithskyhairpin@gmail.com>
2023-07-27 19:56:58 +0900

    add mobile styling

--- app/routes/intro.tsx
+++ app/routes/intro.tsx
@@ -1,7 +1,7 @@
 export const Intro = () => {
   return (
     <div className={"text-center flex flex-col gap-y-2"}>
-      <p>望月文庫是一個數位圖書館，專門提供日本文學作品的翻譯</p>
+      <p>望月文庫是一個數位圖書館，<br className={"lg:hidden"}/>專門提供日本文學作品的翻譯。</p>
       <p>所有提供的翻譯作品，都源於青空文庫列出的無版權限制的文本。</p>
       <p>
         望月文庫的宗旨是讓更多人有機會閱讀和欣賞日本文學作品，進一步感受到日本微妙之美。
